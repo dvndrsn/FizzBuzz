d18b55826f8df092694f86f0e4254379c19f45ca
unknown <dave@dvndrsn.com>
2013-07-28 22:17:28 -0400

    Cleaned up code, added comments

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,11 +1,26 @@
+# FizzBuzz
+# Coded by Dave A. for Hacker School NYC
+# # # # # # # # # # # # # # # # # # # # # # # #
+# For each of the numbers from 1 to 100
+# print Fizz if the number is divisible by 3,
+# Buzz if the number is divisible by 5,
+# FizzBuzz if it is divisible by both, or otherwise,
+# just print the number.
+# # # # # # # # # # # # # # # # # # # # # # # #
+
+# Its fizzbuzz!
 def main():
-    for i in range(1,100+1):
-        printFizzBuzz(i)
-    # test cases
+    # test cases (in case you don't care to loop through all the examples.
     # printFizzBuzz(3)
+    # printFizzBuzz(5)
     # printFizzBuzz(15)
     # printFizzBuzz(1)
 
+    for i in range(1,100+1):
+        printFizzBuzz(i)
+
+
+# prints the appropriate Fizz/Buzz message for the given number.
 def printFizzBuzz(n):
     fizz = (n % 3) == 0
     buzz = (n % 5) == 0
